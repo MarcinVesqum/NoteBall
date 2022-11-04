e217c42d84cc984a0580a04d107523d396982e38
Vesqum <marcin.pelszyk30@gmail.com>
2022-11-04 11:08:42 +0100

    added navigation mobile & descktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
+      colors: {
+        'primary': '#170EA1',
+        'primary-light': '#C0BDFF',
+        'secondary': '#A18700',
+        'btn-color': '#5548FF',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
       spacing: {
         '8xl': '96rem',
         '9xl': '128rem',
       },
       borderRadius: {
         '4xl': '2rem',
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     }
   },
