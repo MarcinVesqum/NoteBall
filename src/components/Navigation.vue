ec3fe89e48182302ce1859b5e651cb5b99feaaac
MarcinVesqum <marcin.pelszyk90@gmail.com>
2022-11-07 11:20:57 +0100

    added EditPage with features and store action

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,7 @@
 <nav class="bg-primary border-gray-200 px-2 sm:px-4 py-2.5  text-white relative">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
       <RouterLink :to="{ name: 'notes'}" class="flex items-center">
-          <span class="self-center text-xl font-semibold whitespace-nowrap">Flowbite</span>
+          <span class="self-center text-xl font-semibold  ">NoteBalls</span>
       </RouterLink>
       <button @click="showMobileMenu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
