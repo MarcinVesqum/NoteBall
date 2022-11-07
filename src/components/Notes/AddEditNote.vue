fff67af9a6a26a0661438d4b3329966d7ec80964
MarcinVesqum <marcin.pelszyk90@gmail.com>
2022-11-07 12:24:48 +0100

    added directive (vAutofocus)

--- src/components/Notes/AddEditNote.vue
+++ src/components/Notes/AddEditNote.vue
@@ -8,6 +8,7 @@ import { default } from '../../composables/useTailwindConfig';
       @input="$emit('update:modelValue', modelValue)"
       :placeholder="placeholder"
       ref="textareaRef"
+      v-autofocus
       rows="4"
       class="block max-h-[120px] p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     
@@ -44,7 +45,14 @@ import { default } from '../../composables/useTailwindConfig';
 */
   const emit = defineEmits(['modelValue'])
 
-
+/*
+  directives
+*/
+const vAutofocus = {
+    mounted(el: any) {
+      el.focus()
+    },
+  }
   
 </script>
 
